1940508146aa1803cb4db60fa8dd745b61eeed09
swichman <stuart.j@wichman-web.org>
2021-07-26 01:11:27 -0600

    db to heatmap functional. autorefresh set on map for web interface

--- cqviewer/__init__.py
+++ cqviewer/__init__.py
@@ -1,3 +1,1 @@
-from .log_parser import wsjtx2df
-from .log_parser import df2heatmap
 from .log_parser import cqDB
--- cqviewer/log_parser.py
+++ cqviewer/log_parser.py
@@ -45,6 +45,7 @@ class cqDB:
             if file_pos > 0:
                 print(f'Last position found at cursor position {file_pos}. Resuming...')
                 self.parse_log()
+                self.create_heatmap(0)
             # if timestamp isn't found, close and open file to reset seek cursor.
             if file_pos < 0:
                 print('No matching timestamp found for last entry. Data must be from different log file.')
@@ -91,80 +92,41 @@ class cqDB:
         else:
             if (max_id - self.db_index) > 0:
                 print(f'{datetime.datetime.now()} ==> {max_id - self.db_index} new entries...')
+                self.create_heatmap(max_id - self.db_index)
         self.db_index = max_id
 
-def wsjtx2df(filename):
-    try:
-        fh = open(str(filename), 'r')
-        fh.close()
-    except:
-        print(f"No such file {filename}... closing\n")
-        exit()
+    def create_heatmap(self, rows):
+        connection = sqlite3.connect(self.db_name)
+        df = pd.read_sql_query("SELECT * from cq_rx", connection)
+        if rows > 0:
+            print(df.tail(rows))
 
-    data = pd.DataFrame()
+        df = df.sort_values('Maidenhead')
+        df = df.reset_index(drop=True)
 
-    # open the WSJT-X log file
-    with open(str(filename), 'r') as fh:
-        lat = 0
-        lon = 0
-        i = 0
-        for line in fh:
-            # remove superfluous spaces, and split string into components
-            fields = ' '.join(line.split()).split(' ')
-            # look for CQ calls
-            if fields[7] != 'CQ':
-                continue
-            maidenhead = fields[len(fields)-1]
-            callsign = fields[len(fields)-2]
-            # ensure that maidenhead is in proper format
-            if len(maidenhead) == 4 or len(maidenhead) == 6:
-                # convert to lat/lon and store to dataframe
-                loc = mh.to_location(maidenhead)
-                data = data.append( pd.DataFrame({"Timestamp":fields[0],
-                                                  "Frequency":fields[1],
-                                                  "Mode":fields[3],
-                                                  "Callsign":callsign,
-                                                  "Maidenhead":maidenhead,
-                                                  "Latitude":loc[0],
-                                                  "Longitude":loc[1],
-                                                  "SNR":fields[4]},
-                                                  index=[i]))
-                i += 1
+        heatmap_df = pd.DataFrame()
 
-    return data
+        old_value = ''
+        avg = 0
 
-def df2heatmap(df, output='', hm_path=''):
-    df = df.sort_values('Maidenhead')
-    df = df.reset_index(drop=True)
+        for index, row in df.iterrows():
+            if row["Maidenhead"] not in old_value and old_value != '' or index == len(df)-1:
+                #print(f'MH: {old_value}    <==>    avg: {avg}')
+                heatmap_df = heatmap_df.append({"weight":round((avg + 25),0),
+                                                "lat":row["Latitude"],
+                                                "lon":row["Longitude"]},
+                                                ignore_index=True
+                                                )
+                avg = int(row["SNR"])
+                pass
+            old_value = row["Maidenhead"]
+            avg = ( avg + int(row["SNR"]) ) / 2
 
-    heatmap_df = pd.DataFrame()
-
-    old_value = ''
-    avg = 0
-
-    for index, row in df.iterrows():
-        if row["Maidenhead"] not in old_value and old_value != '' or index == len(df)-1:
-            #print(f'MH: {old_value}    <==>    avg: {avg}')
-            heatmap_df = heatmap_df.append({"weight":round((avg + 25),0),
-                                            "lat":row["Latitude"],
-                                            "lon":row["Longitude"]},
-                                            ignore_index=True
-                                          )
-            avg = int(row["SNR"])
-            pass
-        old_value = row["Maidenhead"]
-        avg = ( avg + int(row["SNR"]) ) / 2
-
-    if output != '':
-        heatmap_df.to_csv(output + '_heatmap.csv', index=False, sep=';')
-        df.to_csv(output + '_CQ-data.csv', index=False)
-
-    js_leaf_out = '''//heatmap data for spots.
+        js_leaf_out = '''//heatmap data for spots.
 var addressPoints = [
 '''
-    for index, row in heatmap_df.iterrows():
-        js_leaf_out += f'[{row["lat"]},{row["lon"]},{row["weight"]/20}],\n'
-    js_leaf_out = js_leaf_out[:-2] + '\n];'
-    with open(hm_path + '/heatmap.js', 'w') as fh:
-        fh.write(js_leaf_out)
-    return heatmap_df
+        for index, row in heatmap_df.iterrows():
+            js_leaf_out += f'[{row["lat"]},{row["lon"]},{row["weight"]/20}],\n'
+        js_leaf_out = js_leaf_out[:-2] + '\n];'
+        with open('cq-heatmap/heatmap.js', 'w') as fh:
+            fh.write(js_leaf_out)
